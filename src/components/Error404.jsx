251a0254ad1dbce314d117e974b8b06a8d9d6f1d
Na Hyung Choi <schoinh@gmail.com>
2019-09-20 16:43:49 -0700

    style error404 page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import logo from "../assets/images/Logo.png";
 import { Link } from "react-router-dom";
 
 function Error404(props) {
+  var logoStyles = {
+    width: "400px",
+    display: "block",
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginTop: "50px",
+    marginBottom: "2em"
+  };
+
+  var textStyles = {
+    textAlign: "center"
+  };
+
   return (
     <div>
-      <h2>The page {props.location.pathname} does not exist!</h2>
-      <h3>Return to <Link to="/">Home</Link> instead?</h3>
+      <img style={logoStyles} src={logo} />
+      <div style={textStyles}>
+        <h2>Uh oh... This page does not exist!</h2>
+        <h3><Link to="/">Return Home</Link></h3>
+      </div>
     </div>
   );
 }
