eab90d0b87d4152af1e14557039a073ab0492d4a
Na Hyung Choi <schoinh@gmail.com>
2019-09-20 14:29:35 -0700

    update webpack and npm config

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "template.ejs",
       appMountId: "react-app-root",
-      title: "Tap Room",
+      title: "MunchPrep",
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
