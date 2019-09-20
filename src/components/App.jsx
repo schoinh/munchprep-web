c98afd06cd3828b1d569e6696070a1a7f58f8962
Na Hyung Choi <schoinh@gmail.com>
2019-09-20 16:35:48 -0700

    make App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,19 @@
 import React from "react";
-// import { Switch, Route } from "react-router-dom";
-// import { Link } from "react-router-dom";
-// import Header from "./Header";
-// import PropTypes from "prop-types";
-// import imageName from '../assets/images/imageFileName.jpg';
+import { Switch, Route } from "react-router-dom";
+import Error404 from "./Error404";
+import Splash from "./Splash";
+import UserPage from "./UserPage";
 
 function App() {
   return (
     <div className="container">
-      {/* <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-      </Switch> */}
-      {/* <Link to="/">Home</Link> */}
+      <Switch>
+        <Route exact path='/' component={Splash} />
+        <Route path='/user-page' component={UserPage} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
-
-// Ticket.propTypes = {
-//   names: PropTypes.string,
-//   location: PropTypes.string,
-//   issue: PropTypes.string
-// };
 
 export default App;
