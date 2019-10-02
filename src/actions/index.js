d74aefa3879eb9748ff1348d30e1cb721a050264
schoinh <schoinh@gmail.com>
2019-10-02 08:52:20 -0700

    enable firebase toggleChecked

--- src/actions/index.js
+++ src/actions/index.js
@@ -47,33 +47,21 @@ function receiveCategory(categoryIdFromFirebase, categoryFromFirebase, itemsFrom
   };
 }
 
-export function firebaseToggleChecked(categoryId, itemId) {
-  return function (dispatch) {
-    // Look up how to edit data on firebase
-    //
-    // db.collection("categories").orderBy("timestamp")
-    //   .onSnapshot(categoryCollectionSnapshot => {
-    //     categoryCollectionSnapshot.forEach(categorySnapshot => {
-    //       let categoryId = categorySnapshot.id;
-    //       let category = categorySnapshot.data();
-    //       db.collection("categories").doc(categoryId).collection("items").orderBy("timestamp", "desc").onSnapshot(
-    //         itemsCollectionSnapshot => {
-    //           let items = {};
-    //           itemsCollectionSnapshot.forEach(itemSnapshot => {
-    //             items[itemSnapshot.id] = itemSnapshot.data();
-    //           });
-    dispatch(toggleChecked(categoryId, itemId));
-  }
-          );
-});
-      });
-  };
-}
-
 export function toggleChecked(categoryId, itemId) {
-  return {
-    type: c.TOGGLE_CHECKED,
-    categoryId: categoryId,
-    itemId: itemId
+  return function (dispatch) {
+    var itemRef = db.collection("categories").doc(categoryId).collection("items").doc(itemId);
+    console.log("itemRef: ", itemRef);
+    itemRef.get().then(item => {
+      console.log("item.data(): ", item.data());
+      if (item.data().checked == true) {
+        itemRef.update({
+          "checked": false
+        });
+      } else {
+        itemRef.update({
+          "checked": true
+        });
+      }
+    });
   };
 }
