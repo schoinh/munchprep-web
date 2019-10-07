3e3b1a3f90e57def5609e83143fdae1f375fbcf3
schoinh <schoinh@gmail.com>
2019-10-07 08:55:06 -0700

    fix bug that scrambled category order

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,43 +18,44 @@ export function addItem(_name, _categoryId) {
       timestamp: Date.now()
     });
   };
-}
+};
+
 export function startFirebaseComm(userId, userName) {
   return function (dispatch) {
     db.collection("users").doc(userId).get().then((user) => {
       if (user.exists) {
         dispatch(setFirebaseListener(userId));
       } else {
+        var collectionRef = db.collection("users").doc(userId).collection("categories");
         db.collection("users").doc(userId).set({
           name: userName
         })
           .then(() => {
-            var collectionRef = db.collection("users").doc(userId).collection("categories");
             collectionRef.add({
               name: "Produce",
               timestamp: Date.now()
             })
-              .then(() => {
-                collectionRef.add({
-                  name: "Proteins",
-                  timestamp: Date.now() + 1
-                });
-              })
-              .then(() => {
-                collectionRef.add({
-                  name: "Other Foods",
-                  timestamp: Date.now() + 2
-                });
-              })
-              .then(() => {
-                collectionRef.add({
-                  name: "Non-Foods",
-                  timestamp: Date.now() + 3
-                });
-              })
-              .then(() => {
-                dispatch(setFirebaseListener(userId));
-              });
+          })
+          .then(() => {
+            collectionRef.add({
+              name: "Proteins",
+              timestamp: Date.now() + 1
+            });
+          })
+          .then(() => {
+            collectionRef.add({
+              name: "Other Foods",
+              timestamp: Date.now() + 2
+            });
+          })
+          .then(() => {
+            collectionRef.add({
+              name: "Non-Foods",
+              timestamp: Date.now() + 3
+            });
+          })
+          .then(() => {
+            dispatch(setFirebaseListener(userId));
           });
       }
     });
