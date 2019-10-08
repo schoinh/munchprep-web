020ae78d4239f5696966e3a1188b09153ff8c2ea
schoinh <schoinh@gmail.com>
2019-10-08 16:34:05 -0700

    clean up comments

--- __tests__/reducers/shopping-list-reducer.test.js
+++ __tests__/reducers/shopping-list-reducer.test.js
@@ -4,19 +4,6 @@ const { c } = constants;
 
 describe("shoppingListReducer", () => {
   let action;
-  // const sampleShoppingList = {
-  //   1: {
-  //     name: "Produce",
-  //     timestamp: 1000,
-  //     items: {
-  //       11: {
-  //         name: "broccoli",
-  //         checked: false,
-  //         timestamp: 10000
-  //       }
-  //     }
-  //   }
-  // };
 
   test("Should return default state if no action type is recognized", () => {
     expect(shoppingListReducer({}, { type: null })).toEqual({});
