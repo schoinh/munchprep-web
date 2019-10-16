4fddcd38982f105f92fb37bd764e4e4aea2757ab
Na Hyung Choi <schoinh@gmail.com>
2019-10-16 12:11:11 -0700

    unignore firebaseConfig

---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { apiKey } from "./apiKey";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "munchprep.net",
+  databaseURL: "https://munchprep.firebaseio.com",
+  projectId: "munchprep",
+  storageBucket: "munchprep.appspot.com",
+  messagingSenderId: "421066333601",
+  appId: "1:421066333601:web:a5664b306a5158c3d33fe0",
+  measurementId: "G-8PQB1E4FNZ"
+};
+
+export default firebaseConfig;
